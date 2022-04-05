c222b9c86363e832ed4f1078bc54a2754206ba82
Alamgir4597 <rrony45@gmail.com>
2022-04-05 21:18:15 +0600

    link

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Getting Started with Create React App
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This project was bootstrapped with [Bike Review](https://peaceful-brioche-178957.netlify.app/).
 
 ## Available in Website
 ➡️ All bike information are Available.
--- src/Components/NotFouund/NotFound.js
+++ src/Components/NotFouund/NotFound.js
@@ -3,7 +3,8 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div>
-            <h1> NotFound</h1>
+            <h1> Not Found</h1>
+            <img src='https://colorlib.com/cdn-cgi/image/quality=80,format=auto,onerror=redirect,metadata=none/wp/wp-content/uploads/sites/2/404-error-page-templates.jpg' alt="Logo" />;
         </div>
     );
 };
